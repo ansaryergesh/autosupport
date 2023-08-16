136b30cc742ab36cc687176a72ff08ce7a94e91d
Ергешов Ансар <a.yergeshov>
2023-08-16 15:48:59 +0600

    merge conflict resolved

--- src/service/NewQuestion/index.js
+++
@@ -1,9 +0,0 @@
-import { axiosInstanceWithHeader } from '../../api/api.js';
-
-export const getTags = () => {
-  return axiosInstanceWithHeader.get('/api/admin/tags');
-};
-
-export const getKeywords = () => {
-  return axiosInstanceWithHeader.get('/api/admin/keyWords');
-};
