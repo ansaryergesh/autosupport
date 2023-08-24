9c1d4cf6766573cd6115707863738114b4125380
Ергешов Ансар <a.yergeshov>
2023-08-24 14:43:47 +0600

    package.json fixed

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,6 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  build: {
-    rollupOptions: {
-      external: ['react-player'],
-    },
-  },
   css: css(),
   resolve: {
     alias: {
