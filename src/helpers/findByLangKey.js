cc833f3aa5c392f00290e549a8e30c71c367e7a6
Ергешов Ансар <a.yergeshov>
2023-09-17 12:04:38 +0600

    question content changed

--- src/helpers/findByLangKey.js
+++ src/helpers/findByLangKey.js
@@ -1,6 +1,5 @@
 import {getLocale} from "../utils/i18next.js";
 
-// export const findByLangKey = data => data.find(item=> item.langKey === getLocale());
 export const findByLangKey = data => {
   return Array.isArray(data) ? data.find(item=> item.langKey === getLocale()) : [];
 }
