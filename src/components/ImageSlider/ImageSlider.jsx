58835454388d69642f15a4f7bca46efd2d5e44ff
Ергешов Ансар <a.yergeshov>
2023-09-18 13:37:32 +0600

    image fixed

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -3,11 +3,15 @@ import styles from './index.module.less';
 import { ReactComponent as RightArrowButton } from '../../assets/images/RightArrowButton.svg';
 import { ReactComponent as LeftArrowButton } from '../../assets/images/LeftArrowButton.svg';
 import { originAddress } from '../../api/api';
+import {Image} from "antd";
 
 const ImageSlider = ({ slides, sliderData }) => {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
+  const domainName = originAddress;
 
+  const previewImages = sliderData?.map((slide) => `${domainName}${slide.url}`);
+  console.log(previewImages)
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
   };
@@ -19,31 +23,34 @@ const ImageSlider = ({ slides, sliderData }) => {
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
-  const domainName = originAddress;
   return (
-    <section className={styles.slider}>
-      <LeftArrowButton className={styles.leftArrow} onClick={prevSlide} />
-      <RightArrowButton className={styles.rightArrow} onClick={nextSlide} />
-      {sliderData?.map((slide, index) => {
-        return (
-          <div
-            key={index}
-            className={index === current ? `${styles.slide} ${styles.active}` : `${styles.slide}`}
-          >
-            {index === current && (
-              <div>
-                <p className={styles.description}>{slide.description}</p>
-                <img
-                  src={`${domainName}${slide.url}`}
-                  alt={styles.description}
-                  className={styles.image}
-                />
+      <section className={styles.slider}>
+        {sliderData?.map((slide, index) => {
+          return (
+              <div
+                  key={index}
+                  className={index === current ? `${styles.slide} ${styles.active}` : `${styles.slide}`}
+              >
+                {index === current && (
+                    <div style={{position: 'relative'}}>
+                      <LeftArrowButton className={styles.leftArrow} onClick={prevSlide} />
+                      <RightArrowButton className={styles.rightArrow} onClick={nextSlide} />
+                      <p className={styles.description}>{slide.description}</p>
+                      <Image.PreviewGroup
+                          items={previewImages}
+                      >
+                        <Image
+                            src={`${domainName}${slide.url}`}
+                            alt={styles.description}
+                            className={styles.image}
+                        />
+                      </Image.PreviewGroup>
+                    </div>
+                )}
               </div>
-            )}
-          </div>
-        );
-      })}
-    </section>
+          );
+        })}
+      </section>
   );
 };
 
