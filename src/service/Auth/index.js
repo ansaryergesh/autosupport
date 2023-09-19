6e9c787320892ac7e8586b3a8492a661b3e45961
Ергешов Ансар <a.yergeshov>
2023-09-19 18:32:44 +0600

    fix bug

--- src/service/Auth/index.js
+++ src/service/Auth/index.js
@@ -13,9 +13,6 @@ export const clearStorage = () => {
 export const checkerAddress =
   import.meta.env?.MODE === 'development' ? 'http://10.50.216.20' : window.location.origin;
 
-
-
-
 export const onLogin = (data) => {
   return axiosInstance.post('/api/authenticate', data);
 };
