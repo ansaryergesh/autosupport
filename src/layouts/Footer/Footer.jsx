0772e2f704a4f6c84dbcc6097f907288f5c6f601
Ергешов Ансар <a.yergeshov>
2023-09-27 13:17:18 +0600

    confidential

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './index.module.less';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>Политика конфиденциальности</p>
+      <p><a style={{color: '@gray-color-1 !important'}} href={'/confidential.pdf'} target={'_blank'} rel="noreferrer">Политика конфиденциальности</a></p>
       <p>050040, г. Алматы, проспект Аль-Фараби 77/7, БЦ Esentai Tower: 3, 7 этаж</p>
       <p>© 2023 АО «Фридом Финанс»</p>
     </div>
